4de446ea966a1d08b366459b145f61c3eb287400
yh54321 <yotamhod2@gmail.com>
2023-01-03 20:12:04 +0200

    Some fixes

--- src/PokerChip.d.ts
+++ src/PokerChip.d.ts
@@ -1,1 +1,17 @@
-declare module 'react-pokerchip'; 
+declare module 'react-pokerchip' {
+    import * as React from 'react';
+
+    type PokerChipProps = {
+        size?: number,
+        value?: number,
+        text?: string,
+        currency?: string,
+        color?: string,
+        lineColor?: string,
+        onClick?: Function,
+        disabled?: boolean
+    };
+
+    const PokerChip: React.FC<PokerChipProps>;
+    export default PokerChip;
+}
